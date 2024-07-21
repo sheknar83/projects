d67f16595de05bf07bf2541bf75b503c6128501c
sheknar83 <sheknar83@gmail.com>
2024-07-21 17:17:29 +0530

    !!!!!<>!!!!

--- sample-spring-microservices-kubernetes/admin-service/src/main/java/pl/piomin/services/admin/AdminApplication.java
+++ sample-spring-microservices-kubernetes/admin-service/src/main/java/pl/piomin/services/admin/AdminApplication.java
@@ -6,11 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-@SpringBootApplication
-@EnableAdminServer
 @EnableDiscoveryClient
+@EnableAdminServer
 @EnableScheduling
+@SpringBootApplication
 public class AdminApplication {
 
 	public static void main(String[] args) {
